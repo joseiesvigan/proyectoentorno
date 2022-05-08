4bd77eda5749d4742235b1bf95330f5668e0ba7b
jose <jrdmchepu@gmail.com>
2022-05-08 09:33:33 +0100

    Initial commit

---
+++ proyectoentorno/src/cuenta/Main.java
@@ -0,0 +1,31 @@
+package cuenta;
+public class Main {
+    private static java.lang.Object System;
+
+    public static void main(String[] args)
+    {
+        CCuenta miCuenta;
+        double saldoActual;
+
+        miCuenta = new CCuenta("Juan López","1000-2365-85-1230456789", 2500, 0);
+        saldoActual = miCuenta.estado();
+            System.out.println("El saldo actual es " + saldoActual);
+
+        try {
+            System.out.println("Cargo en cuenta");
+            miCuenta.retirar(2300);
+            System.out.println("El nuevo saldo es: " + miCuenta.estado());
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+
+        try {
+            System.out.println("Abono en cuenta");
+            miCuenta.ingresar(695);
+            System.out.println("El nuevo saldo es: " + miCuenta.estado());
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+}
